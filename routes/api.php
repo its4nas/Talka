e302927b0b5d62a181434ad72e58efacd67a5db9
Anas Al-Dharei <anas202255@gmail.com>
2023-06-21 11:36:12 +0300

    resful api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+
+use App\Http\Controllers\API\AuthUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -9,11 +10,25 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return auth()->user();
 });
+
+Route::post('/register', [AuthUserController::class, 'register'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthUserController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
+
+
+Route::post('/logout', [AuthUserController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
